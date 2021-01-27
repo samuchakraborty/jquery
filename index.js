5b3b531f8fcdb0980cb035af090bc43061c1dbb7
samuchakraborty <samuckb@gmail.com>
2021-01-27 19:26:05 +0600

    project init

---
+++ index.js
@@ -0,0 +1,7 @@
+$("h1").addClass("big-Note margin-30");
+
+$("button").html("<em><Strong>don't click me</strong></em>");
+
+$("img").attr("src", "images/sss.jpeg");
+
+$("a").attr("href", "https://www.yahoo.com");
